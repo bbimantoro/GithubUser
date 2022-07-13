6af7530618ddc3a3717b1e572fba6c6ae7ac3edd
Bimantoro <muhammad_s1sisfo2020@mahasiswa.ung.ac.id>
2022-07-13 08:18:26 +0800

    add scrollview

--- app/src/main/java/com/example/githubuser/ListUserAdapter.kt
+++ app/src/main/java/com/example/githubuser/ListUserAdapter.kt
@@ -3,7 +3,6 @@ package com.example.githubuser
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.githubuser.databinding.ItemRowUserBinding
 
 class ListUserAdapter(private val listUser: ArrayList<User>) :
